3ed348d50335f26d0095c72c5c36f6ea0151d6f3
Aleksandr Nomokonov <aleks.agn@gmail.com>
2024-10-03 22:01:15 +0900

    Thirteen commit of Playlist Maker Application (implemented saving the user's chosen theme (light/dark) in Shared Preferences)

---
+++ app/src/main/java/com/aleksagn/playlistmaker/App.kt
@@ -0,0 +1,38 @@
+package com.aleksagn.playlistmaker
+
+import android.app.Application
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatDelegate
+
+const val PLAYLIST_MAKER_PREFERENCES = "playlist_maker_preferences"
+const val DAY_NIGHT_THEME_KEY = "key_for_day_night_theme"
+
+class App : Application() {
+    var darkTheme = false
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val sharedPreferences = getSharedPreferences(PLAYLIST_MAKER_PREFERENCES, MODE_PRIVATE)
+        darkTheme = sharedPreferences.getBoolean(DAY_NIGHT_THEME_KEY, false)
+
+        switchTheme(darkTheme)
+    }
+
+    fun switchTheme(darkThemeEnabled: Boolean) {
+        val sharedPreferences = getSharedPreferences(PLAYLIST_MAKER_PREFERENCES, MODE_PRIVATE)
+        darkTheme = darkThemeEnabled
+
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkThemeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+
+        sharedPreferences.edit()
+            .putBoolean(DAY_NIGHT_THEME_KEY, darkTheme)
+            .apply()
+    }
+}
