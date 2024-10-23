b3e6c5e13b45601d52a62dcb9b62c7b2cf101ebd
Aleksandr Nomokonov <aleks.agn@gmail.com>
2024-10-24 01:32:15 +0900

    Seventeenth commit (add incorrect results filtering of search response)

--- app/src/main/java/com/aleksagn/playlistmaker/Track.kt
+++ app/src/main/java/com/aleksagn/playlistmaker/Track.kt
@@ -7,7 +7,6 @@ data class Track(
     val trackId: Int, // Идентификатор трека
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
-    var trackTime: String, // Продолжительность трека
     val trackTimeMillis: Long, // Продолжительность трека в миллисекундах
     val artworkUrl100: String, // Ссылка на изображение обложки
     val collectionName: String, // Название альбома
