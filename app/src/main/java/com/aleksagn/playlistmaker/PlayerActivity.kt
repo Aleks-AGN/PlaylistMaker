d3d57ddb27c52dd3354d1a8dd5c123ceda23c508
Aleksandr Nomokonov <aleks.agn@gmail.com>
2025-01-13 02:17:14 +0900

    Eighteenth commit (realized debounce, progressbar and mediaplayer)

--- app/src/main/java/com/aleksagn/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/aleksagn/playlistmaker/PlayerActivity.kt
@@ -1,8 +1,12 @@
 package com.aleksagn.playlistmaker
 
 import android.content.Context
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.TypedValue
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,8 +15,28 @@ import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.gson.Gson
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class PlayerActivity : AppCompatActivity() {
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+        private const val DELAY = 250L
+    }
+
+    private var playerState = STATE_DEFAULT
+
+    private var mediaPlayer = MediaPlayer()
+    private lateinit var playButton: ImageButton
+    private lateinit var pauseButton: ImageButton
+    private lateinit var currentTrackTime: TextView
+    private var previewUrl: String = ""
+    private var mainThreadHandler: Handler? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
@@ -26,13 +50,23 @@ class PlayerActivity : AppCompatActivity() {
 
         val trackName = findViewById<TextView>(R.id.track_name)
         val artistName = findViewById<TextView>(R.id.artist_name)
-        val currentTrackTime = findViewById<TextView>(R.id.current_track_time)
+        playButton = findViewById(R.id.btn_play)
+        pauseButton = findViewById(R.id.btn_pause)
+        currentTrackTime = findViewById(R.id.current_track_time)
 
         val trackTime = findViewById<TextView>(R.id.track_time)
         val collectionName = findViewById<TextView>(R.id.collection_name)
         val releaseDate = findViewById<TextView>(R.id.release_date)
         val primaryGenreName = findViewById<TextView>(R.id.primary_genre_name)
         val country = findViewById<TextView>(R.id.country)
+
+        playButton.setOnClickListener {
+            startPlayer()
+        }
+
+        pauseButton.setOnClickListener {
+            pausePlayer()
+        }
 
         val intent = intent
         val jsonTrack = intent.getStringExtra("track").toString()
@@ -43,7 +77,7 @@ class PlayerActivity : AppCompatActivity() {
         artistName.text = track.artistName
 
         trackTime.text = track.getFormatTime()
-        currentTrackTime.text = trackTime.text
+        //currentTrackTime.text = trackTime.text
 
         if (track.collectionName.isNullOrEmpty()) {
             collectionName.isVisible = false
@@ -57,6 +91,8 @@ class PlayerActivity : AppCompatActivity() {
         primaryGenreName.text = track.primaryGenreName
         country.text = track.country
 
+        previewUrl = track.previewUrl
+
         Glide.with(applicationContext)
             .load(track.getCoverArtwork())
             .placeholder(R.drawable.ic_track_placeholder)
@@ -64,6 +100,70 @@ class PlayerActivity : AppCompatActivity() {
             .centerCrop()
             .transform(RoundedCorners(dpToPx(8f,this.applicationContext)))
             .into(cover)
+
+        mainThreadHandler = Handler(Looper.getMainLooper())
+
+        preparePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mainThreadHandler?.removeCallbacks(updateCurrentPlayTime())
+        mediaPlayer.release()
+    }
+
+    private fun preparePlayer() {
+        mediaPlayer.setDataSource(previewUrl)
+
+        mediaPlayer.prepareAsync()
+
+        mediaPlayer.setOnPreparedListener {
+            playButton.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+
+        mediaPlayer.setOnCompletionListener {
+            playButton.isVisible = true
+            pauseButton.isVisible = false
+            playerState = STATE_PREPARED
+            mainThreadHandler?.removeCallbacks(updateCurrentPlayTime())
+            currentTrackTime.text = "00:00"
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playButton.isVisible = false
+        pauseButton.isVisible = true
+        playerState = STATE_PLAYING
+        mainThreadHandler?.post(updateCurrentPlayTime())
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playButton.isVisible = true
+        pauseButton.isVisible = false
+        playerState = STATE_PAUSED
+        mainThreadHandler?.removeCallbacks(updateCurrentPlayTime())
+    }
+
+    private fun updateCurrentPlayTime(): Runnable {
+        return object : Runnable {
+            override fun run() {
+                if (playerState == STATE_PLAYING) {
+                    currentTrackTime.text = SimpleDateFormat(
+                        "mm:ss",
+                        Locale.getDefault()
+                    ).format(mediaPlayer.currentPosition)
+                    mainThreadHandler?.postDelayed(this, DELAY)
+                }
+            }
+        }
     }
 
     fun dpToPx(dp: Float, context: Context): Int {
--- app/src/main/java/com/aleksagn/playlistmaker/Track.kt
+++ app/src/main/java/com/aleksagn/playlistmaker/Track.kt
@@ -12,7 +12,8 @@ data class Track(
     val collectionName: String, // Название альбома
     val releaseDate: String, // Год релиза трека
     val primaryGenreName: String, // Жанр трека
-    val country: String // Страна исполнителя
+    val country: String, // Страна исполнителя
+    val previewUrl: String // Ссылка на отрывок трека
 ) {
     fun getFormatTime() = SimpleDateFormat("mm:ss", Locale.getDefault()).format(trackTimeMillis)
 
